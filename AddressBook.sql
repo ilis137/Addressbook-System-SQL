e93e5f2058fca4ae5be2c9e3c5e8daa23d392d05
ilis137 <iliyasshahapure1@gmail.com>
2022-10-22 08:58:39 +0530

    add:create table addressbook

--- AddressBook.sql
+++ AddressBook.sql
@@ -8,3 +8,17 @@ use addressbook;
 
 select database();
 
+create table addressbook(
+     id       int unsigned not null auto_increment,
+     first_name       varchar(10),
+     last_name        varchar(10),
+     address  varchar(50),
+     city     varchar(10),
+     state    varchar(20),
+     zip      int ,
+     phone_number     int,
+     email    varchar(30),
+     primary key (id)
+);
+
+describe addressbook;
